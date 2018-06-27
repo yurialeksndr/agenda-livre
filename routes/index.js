909a4071ef1aed194bafc00db3e971614414a264
yurialeksndr <yuriatp@gmail.com>
2018-06-27 20:55:17 -0300

    Cria as rotas e página para adicionar novas entradas no DB.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ var router = express.Router();
 var model = require("./../model/tasks")();
 
 /* GET home page. */
-router.get('/', function(request, response, next) {
+router.get("/", function(request, response, next) {
   model.find(null, function(error, tasks) {
     if (error) {
       throw error;
     }
-    response.render('index', { title: 'Agenda Livre', tasks: tasks });
+    response.render("index", { title: "Agenda Livre", tasks: tasks });
+  });
+});
+
+router.get("/new", function(request, response, next) {
+  response.render("new", {title: "Agenda Livre"});
+});
+
+router.post("/new", function(request, response, next) {
+  var data = request.body;
+
+  model.create(data, function(error, task) {
+    if (error) {
+      throw error;
+    }
+    response.redirect("/");
   });
 });
 
