e6b7e5a611566728a509dbea7237d4f5aff2703c
yurialeksndr <yuriatp@gmail.com>
2018-07-01 16:49:19 -0300

    Mostra na página somente as entradas do DB correspondentes aos dois laboratórios por dia do Agenda Livre.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var model = require("./../model/tasks")();
 
+var title = "AGENDA LIVRE"
+
+labs_by_day = [
+  ["", ""],                   //SUNDAY    - DOMINGO
+  ["heliopolis", "itaquera"], //MONDAY    - SEGUNDA
+  ["itororo", "anhanguera"],  //TUESDAY   - TERÇA
+  ["jockey", "guarapiranga"], //WEDNESDAY - QUARTA
+  ["ccsp", "penha"],          //THURSDAY  - QUINTA
+  ["tiradentes", "olido"],    //FRIDAY    - SEXTA
+  ["ccj", "tres_pontes"]      //SATURDAY  - SÁBADO
+]
+
 /* GET home page. */
 router.get("/", function(request, response, next) {
-  model.find(null, function(error, tasks) {
+
+  var date = new Date();
+
+  model.find({ lab : [labs_by_day[date.getDay()][0], labs_by_day[date.getDay()][1]] }, function(error, tasks) {
     if (error) {
       throw error;
     }
-    response.render("index", { title: "Agenda Livre", tasks: tasks });
+    response.render("index", { title: title, tasks: tasks });
   });
 });
 
 router.get("/new", function(request, response, next) {
-  response.render("new", {title: "Agenda Livre"});
+  response.render("new", {title: title});
 });
 
 router.post("/new", function(request, response, next) {
